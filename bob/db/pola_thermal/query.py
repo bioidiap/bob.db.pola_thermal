19ccfb63383847157d5b2fe71bdf731f9946feb8
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2017-10-24 09:33:16 +0200

    Added modality in the query

--- bob/db/pola_thermal/query.py
+++ bob/db/pola_thermal/query.py
@@ -37,6 +37,14 @@ class Database(bob.db.base.SQLiteDatabase):
         # call base class constructors to open a session to the database
 
         super(Database, self).__init__(SQLITE_FILE, File, original_directory, original_extension)
+        
+    @property
+    def modality_separator(self):
+        return "VIS"
+
+    @property
+    def modalities(self):
+        return ['VIS', 'THERMAL']
 
     def protocols(self):
         return PROTOCOLS
@@ -59,7 +67,7 @@ class Database(bob.db.base.SQLiteDatabase):
         """
         return file.annotations(annotation_type=annotation_type)
 
-    def objects(self, groups=None, protocol=None, purposes=None, model_ids=None, **kwargs):
+    def objects(self, groups=None, protocol=None, purposes=None, model_ids=None, modality=None, **kwargs):
         """
           This function returns lists of File objects, which fulfill the given restrictions.
         """
@@ -68,6 +76,8 @@ class Database(bob.db.base.SQLiteDatabase):
         groups = self.check_parameters_for_validity(groups, "group", GROUPS)
         protocols = self.check_parameters_for_validity(protocol, "protocol", PROTOCOLS)
         purposes = self.check_parameters_for_validity(purposes, "purpose", PURPOSES)
+        modality = self.check_parameters_for_validity(
+            modality, "modality", self.modalities)
 
         # You need to select only one protocol
         if (len(protocols) > 1):
@@ -81,6 +91,8 @@ class Database(bob.db.base.SQLiteDatabase):
         query = query.filter(bob.db.pola_thermal.Protocol_File_Association.group.in_(groups))
         query = query.filter(bob.db.pola_thermal.Protocol_File_Association.protocol.in_(protocols))
         query = query.filter(bob.db.pola_thermal.Protocol_File_Association.purpose.in_(purposes))
+        query = query.filter(bob.db.pola_thermal.File.modality.in_(modality))
+        
 
         if model_ids is not None and not "probe" in purposes:
             if type(model_ids) is not list and type(model_ids) is not tuple:
